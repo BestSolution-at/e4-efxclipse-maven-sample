dff62139ba6c68b77d82b67a97c7443d9e063995
Tom Schindl <tom.schindl@bestsolution.at>
2017-08-21 20:29:35 +0200

    adding netbeans support

--- sample.mvn.parent/sample.mvn.app/src/main/java/sample/mvn/app/HelloMaven.java
+++ sample.mvn.parent/sample.mvn.app/src/main/java/sample/mvn/app/HelloMaven.java
@@ -2,12 +2,21 @@ package sample.mvn.app;
 
 import javax.annotation.PostConstruct;
 
+import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import sample.mvn.lib.HelloWorldHelper;
 
 public class HelloMaven {
 	@PostConstruct
 	void init(BorderPane p) {
-		p.setCenter(new Button("Hello Maven!"));
+            Button b = new Button("Hello Maven!");
+            b.setOnAction( this::handleButtonAction );
+            p.setCenter(b);
 	}
+        
+        void handleButtonAction(ActionEvent e) {
+            System.err.println("Action: " + e);
+            HelloWorldHelper.helloWorld();
+        }
 }
---
+++ sample.mvn.parent/sample.mvn.lib/src/main/java/sample/mvn/lib/HelloWorldHelper.java
@@ -0,0 +1,7 @@
+package sample.mvn.lib;
+
+public class HelloWorldHelper {
+	public static void helloWorld() {
+		System.err.println("Hello World from Helper!!");
+	}
+}
