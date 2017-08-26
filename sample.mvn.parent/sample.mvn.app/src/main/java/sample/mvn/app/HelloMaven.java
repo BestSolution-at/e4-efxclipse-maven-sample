664d493de64715a5c4be2cf5b4e04f85cfe410c0
Tom Schindl <tom.schindl@bestsolution.at>
2017-08-26 17:57:36 +0200

    add back warning/error samples

--- sample.mvn.parent/sample.mvn.app/src/main/java/sample/mvn/app/HelloMaven.java
+++ sample.mvn.parent/sample.mvn.app/src/main/java/sample/mvn/app/HelloMaven.java
@@ -2,14 +2,26 @@ package sample.mvn.app;
 
 import javax.annotation.PostConstruct;
 
+// This is ok because packag is exported
+//import org.eclipse.e4.core.services.adapter.Adapter;
+
+// FIXME Should give a warning/error
+//import org.eclipse.fx.core.internal.JUtilLoggerFactory;
+
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import sample.mvn.lib.HelloWorldHelper;
+// FIXME Should give a warning/error
+//import sample.mvn.lib.internal.InternalAPI;
 
 public class HelloMaven {
+//	JUtilLoggerFactory f;
+	
 	@PostConstruct
 	void init(BorderPane p) {
+//		Adapter a;
+//		InternalAPI i;
 		Button b = new Button("Hello Maven!");
 		b.setOnAction(this::handleButtonAction);
 		p.setCenter(b);
